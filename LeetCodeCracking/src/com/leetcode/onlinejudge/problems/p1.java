27362bfbacd82b60aa83ae2c2e5f6b307c42ebc4
laonawuli <laonawuli@126.com>
2014-06-24 12:19:11 +0800

    Code

--- LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p1.java
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p1.java
@@ -23,6 +23,22 @@ public class p1 extends BaseProblem implements IProblem {
 
     @Override
     public void run() {
-        print("TEST");
+        String s = "    the        sky      is       blue    ";
+        print("The original words is: " + s);
+        print("The reversed words is: " + new Solution().reverseWords(s));
+    }
+
+    public class Solution {
+        public String reverseWords(String s) {
+            String[] words = s.trim().split(" ");
+            String done = "";
+            for (int i = words.length - 1; i >= 0; i--) {
+                if (words[i].equals("")) {
+                    continue;
+                }
+                done += words[i] + " ";
+            }
+            return done.trim();
+        }
     }
 }
