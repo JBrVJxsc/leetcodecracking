99b318fec34fc11eb2f288ace2b1065ad4fc9018
laonawuli <laonawuli@126.com>
2014-06-24 16:36:34 +0800

    Code

--- LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p3.java
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p3.java
@@ -1,7 +1,12 @@
 package com.leetcode.onlinejudge.problems;
 
+import com.leetcode.base.Parameter;
+import com.leetcode.common.Random;
 import com.leetcode.interfaces.IProblem;
 import com.leetcode.onlinejudge.BaseProblem;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Created by Who on 2014/6/24.
@@ -19,28 +24,131 @@ public class p3 extends BaseProblem implements IProblem {
 
     @Override
     public void run() {
-
+        Parameter<Integer> number = new Parameter<Integer>(1000);
+        set(number);
+        Point[] points = getPoints(number.getValue());
+        print(new Solution().maxPoints(points));
     }
 
+    private Point[] getPoints(int number) {
+        ArrayList<Point> pointArrayList = new ArrayList<Point>();
+        Point[] points = new Point[number];
+        int index = 0;
+        int range = number;
+        while (index != points.length) {
+            Point point = new Point(Random.getInt(range), Random.getInt(range));
+            //Loop, until get an unique point.
+            while (pointArrayList.contains(point)) {
+                point = new Point(Random.getInt(range), Random.getInt(range));
+            }
+            points[index] = point;
+            pointArrayList.add(point);
+            index++;
+        }
+        return points;
+    }
 
     public class Solution {
-        public int maxPoints(Point[] points) {
-            return 0;
+
+        private int getMaximum(ArrayList<PointPlus> pointPlusArrayList) {
+            int max = 0;
+            double flag = Double.MIN_VALUE;
+            int counter = 0;
+            for (int i = 0; i < pointPlusArrayList.size(); i++) {
+                if (flag != pointPlusArrayList.get(i).getSlope()) {
+                    if (counter > max) {
+                        max = counter;
+                    }
+                    flag = pointPlusArrayList.get(i).getSlope();
+                    counter = 1;
+                } else {
+                    counter++;
+                }
+            }
+            if (counter > max) {
+                max = counter;
+            }
+            return max;
         }
 
-        public class Point {
-            int x;
-            int y;
+        public int maxPoints(Point[] points) {
+            int maxNumber = 0;
+            ArrayList<PointPlus> pointPlusArrayList = new ArrayList<PointPlus>();
+            for (int i = 0; i < points.length; i++) {
+                pointPlusArrayList.add(new PointPlus(points[i]));
+            }
+            for (int i = 0; i < points.length; i++) {
+                pointPlusArrayList.sort(new Comparator<PointPlus>() {
 
-            public Point() {
-                x = 0;
-                y = 0;
+                    private Point point;
+
+                    private Double getSlope(PointPlus pointPlus) {
+                        if (this.point.x == pointPlus.getPoint().x) {
+                            if (this.point.y > pointPlus.getPoint().y) {
+                                return Double.NEGATIVE_INFINITY;
+                            } else if (this.point.y < pointPlus.getPoint().y) {
+                                return Double.NEGATIVE_INFINITY;
+                            } else {
+                                return Double.MIN_VALUE;
+                            }
+                        }
+                        pointPlus.setSlope((double) (pointPlus.getPoint().y - this.point.y) / (double) (pointPlus.getPoint().x - this.point.x));
+                        return pointPlus.getSlope();
+                    }
+
+                    public Comparator setPoint(Point point) {
+                        this.point = point;
+                        return this;
+                    }
+
+                    @Override
+                    public int compare(PointPlus o1, PointPlus o2) {
+                        return getSlope(o1).compareTo(getSlope(o2));
+                    }
+                }.setPoint(points[i]));
+
+                int number = getMaximum(pointPlusArrayList);
+                if (number > maxNumber) {
+                    maxNumber = number;
+                }
+            }
+            return maxNumber;
+        }
+
+        public class PointPlus {
+            private Point point = null;
+            private double slope = 0d;
+
+            public PointPlus(Point point) {
+                this.point = point;
             }
 
-            public Point(int a, int b) {
-                x = a;
-                y = b;
+            public Point getPoint() {
+                return point;
             }
+
+            public double getSlope() {
+                return slope;
+            }
+
+            public void setSlope(double slope) {
+                this.slope = slope;
+            }
+        }
+    }
+
+    private class Point {
+        private int x;
+        private int y;
+
+        public Point() {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(int a, int b) {
+            x = a;
+            y = b;
         }
     }
 }
