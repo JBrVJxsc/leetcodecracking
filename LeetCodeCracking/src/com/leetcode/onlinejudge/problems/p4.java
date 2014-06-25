836f964167dde204ab6949e16e5d91ffc68892f2
laonawuli <laonawuli@126.com>
2014-06-25 13:31:21 +0800

    Code

---
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p4.java
@@ -0,0 +1,24 @@
+package com.leetcode.onlinejudge.problems;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+
+/**
+ * Created by Who on 2014/6/25.
+ */
+public class p4 extends BaseProblem implements IProblem {
+    @Override
+    public String getName() {
+        return "Sort List ";
+    }
+
+    @Override
+    public String getSummary() {
+        return "Sort a linked list in O(n log n) time using constant space complexity.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
