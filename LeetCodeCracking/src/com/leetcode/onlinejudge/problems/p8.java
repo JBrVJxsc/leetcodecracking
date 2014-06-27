a7e6b434b7b14901a45be47c40d3e57f5b3a2c87
laonawuli <laonawuli@126.com>
2014-06-27 14:51:49 +0800

    Code

--- LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p8.java
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p8.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Who on 2014/6/25.
+ * Created by Who on 2014/6/27.
  */
-public class p7 extends BaseProblem implements IProblem {
+public class p8 extends BaseProblem implements IProblem {
     @Override
     public String getName() {
-        return "Binary Tree Postorder Traversal";
+        return "Binary Tree Preorder Traversal";
     }
 
     @Override
     public String getSummary() {
-        return "Given a binary tree, return the postorder traversal of its nodes' values.\n" +
+        return "Given a binary tree, return the preorder traversal of its nodes' values.\n" +
                 "For example:\n" +
                 "Given binary tree {1,#,2,3},\n" +
                 "   1\n" +
@@ -25,7 +25,7 @@ public class p7 extends BaseProblem implements IProblem {
                 "     2\n" +
                 "    /\n" +
                 "   3\n" +
-                "return [3,2,1].\n" +
+                "return [1,2,3].\n" +
                 "Note: Recursive solution is trivial, could you do it iteratively?";
     }
 
@@ -35,8 +35,9 @@ public class p7 extends BaseProblem implements IProblem {
         root.right = new TreeNode(2);
         root.left = new TreeNode(4);
         root.right.left = new TreeNode(3);
-        print(new Solution().postorderTraversal(root));
+        print(new Solution().preorderTraversal(root));
     }
+
 
     public class Solution {
 
@@ -44,16 +45,16 @@ public class p7 extends BaseProblem implements IProblem {
             if (node == null) {
                 return;
             }
+            list.add(node.val);
             if (node.left != null) {
                 get(list, node.left);
             }
             if (node.right != null) {
                 get(list, node.right);
             }
-            list.add(node.val);
         }
 
-        public List<Integer> postorderTraversal(TreeNode root) {
+        public List<Integer> preorderTraversal(TreeNode root) {
             List<Integer> list = new ArrayList<Integer>();
             get(list, root);
             return list;
