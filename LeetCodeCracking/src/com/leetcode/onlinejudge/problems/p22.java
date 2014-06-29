d6a8a86ff007dd3fdbdaadb265d3165809edd137
laonawuli <laonawuli@126.com>
2014-06-29 20:50:51 +0800

    Code

--- LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p22.java
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p22.java
@@ -21,12 +21,37 @@ public class p22 extends BaseProblem implements IProblem {
 
     @Override
     public void run() {
-
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.left.right = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.right.left = new TreeNode(7);
+        root.right.right.left.left = new TreeNode(8);
+        root.left.left.left = new TreeNode(9);
+        print(new Solution().maxDepth(root));
     }
 
     public class Solution {
+
+        private int getMaxDepth(TreeNode node, int depth) {
+            int left = depth;
+            int right = depth;
+            if (node.left != null) {
+                left = getMaxDepth(node.left, depth + 1);
+            }
+            if (node.right != null) {
+                right = getMaxDepth(node.right, depth + 1);
+            }
+            return (left < right) ? right : left;
+        }
+
         public int maxDepth(TreeNode root) {
-            return 0;
+            if (root == null) {
+                return 0;
+            }
+            return getMaxDepth(root, 1);
         }
     }
 }
