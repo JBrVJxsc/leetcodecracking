889cb1cc23b61747a05520ca9fc725ebbe500fc4
laonawuli <laonawuli@126.com>
2014-06-30 11:55:06 +0800

    Code

--- LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p26.java
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/problems/p26.java
@@ -3,13 +3,16 @@ package com.leetcode.onlinejudge.problems;
 import com.leetcode.interfaces.IProblem;
 import com.leetcode.onlinejudge.BaseProblem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Who on 2014/6/29.
  */
 public class p26 extends BaseProblem implements IProblem {
     @Override
     public String getName() {
-        return "Best Time to Buy and Sell Stock II";
+        return "[UNSOLVED: TLE]Best Time to Buy and Sell Stock II";
     }
 
     @Override
@@ -20,12 +23,72 @@ public class p26 extends BaseProblem implements IProblem {
 
     @Override
     public void run() {
-
+        int[] prices = new int[]{10, 21, 13, 41, 50, 16, 27, 10, 39};
+        //prices = new int[]{1, 3, 2, 1};
+        print(new Solution().maxProfit(prices));
     }
 
     public class Solution {
         public int maxProfit(int[] prices) {
-            return 0;
+            ArrayList<Profit> profitArrayList = new ArrayList<Profit>();
+            ArrayList<Price> priceArrayList = new ArrayList<Price>();
+            for (int i = 0; i < prices.length; i++) {
+                priceArrayList.add(new Price(i, prices[i]));
+            }
+            for (int i = 0; i < prices.length; i++) {
+                for (int j = i + 1; j < prices.length; j++) {
+                    if (prices[i] < prices[j]) {
+                        profitArrayList.add(new Profit(priceArrayList.get(i), priceArrayList.get(j)));
+                    }
+                }
+            }
+            Collections.sort(profitArrayList);
+            int sum = 0;
+            for (Profit profit : profitArrayList) {
+                sum += profit.useProfit();
+            }
+            return sum;
+        }
+
+        public class Price {
+            public int index = 0;
+            public int price = 0;
+            public boolean used = false;
+
+            public Price(int index, int price) {
+                this.index = index;
+                this.price = price;
+            }
+        }
+
+        public class Profit implements Comparable<Profit> {
+            public Price low = null;
+            public Price high = null;
+
+            private int getProfit() {
+                return high.price - low.price;
+            }
+
+            public Profit(Price low, Price high) {
+                this.low = low;
+                this.high = high;
+            }
+
+            public int useProfit() {
+                if (low.used || high.used) {
+                    return 0;
+                } else {
+                    print(low.price + ", " + high.price);
+                    low.used = true;
+                    high.used = true;
+                    return high.price - low.price;
+                }
+            }
+
+            @Override
+            public int compareTo(Profit o) {
+                return Integer.compare(o.getProfit(), getProfit());
+            }
         }
     }
 }
