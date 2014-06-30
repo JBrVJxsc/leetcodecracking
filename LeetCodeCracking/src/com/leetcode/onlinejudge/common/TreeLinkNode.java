d8f8ff56c93cd2b95640690e7d56b4b74fab2934
laonawuli <laonawuli@126.com>
2014-06-30 15:14:56 +0800

    Code

---
+++ LeetCodeCracking/src/com/leetcode/onlinejudge/common/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package com.leetcode.onlinejudge.common;
+
+/**
+ * Created by Who on 2014/6/30.
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+
+    public TreeLinkNode(int x) {
+        val = x;
+    }
+}
