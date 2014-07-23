47e51139a97b8e3295b9cae107c9e267f3a698a4
laonawuli <laonawuli@126.com>
2014-07-23 11:10:04 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_1_Remove_Duplicates_from_Sorted_Array.java
+++ src/com/leetcode/onlinejudge/problems/_1_Remove_Duplicates_from_Sorted_Array.java
@@ -18,6 +18,60 @@ public class _1_Remove_Duplicates_from_Sorted_Array extends BaseProblem implemen
 
     @Override
     public void run() {
+        int[] a = new int[]{0, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6};
+        a = new int[]{1, 1, 2};
+        print(new Solution3().removeDuplicates(a));
+    }
 
+    public class Solution {
+        public int removeDuplicates(int[] A) {
+            if (A == null || A.length == 0) {
+                return 0;
+            }
+            int front = A[0];
+            int currentPointer = 1;
+            for (int i = 1; i < A.length; i++) {
+                if (A[i] != front) {
+                    front = A[currentPointer] = A[i];
+                    currentPointer++;
+                }
+            }
+            return currentPointer;
+        }
+    }
+
+    public class Solution1 {
+        public int removeDuplicates(int[] A) {
+            if (A == null || A.length == 0)
+                return 0;
+            int pointer = 0;
+            for (int i = 1; i < A.length; i++) {
+                if (A[pointer] != A[i])
+                    A[++pointer] = A[i];
+            }
+            return pointer + 1;
+        }
+    }
+
+    public class Solution2 {
+        public int removeDuplicates(int[] A) {
+            if (A == null || A.length == 0) return 0;
+            int pointer = 0;
+            for (int i = 1; i < A.length; i++)
+                if (A[pointer] != A[i])
+                    A[++pointer] = A[i];
+            return pointer + 1;
+        }
+    }
+
+    public class Solution3 {
+        public int removeDuplicates(int[] A) {
+            if (A == null || A.length == 0) return 0;
+            int index = 0;
+            for (int i = 1; i < A.length; i++)
+                if (A[index] != A[i])
+                    A[++index] = A[i];
+            return index + 1;
+        }
     }
 }
