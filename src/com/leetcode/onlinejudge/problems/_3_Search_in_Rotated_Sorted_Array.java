ad1344c2480d9dbe5979eba6df1651cb046f20f3
laonawuli <laonawuli@126.com>
2014-07-23 17:29:56 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_3_Search_in_Rotated_Sorted_Array.java
+++ src/com/leetcode/onlinejudge/problems/_3_Search_in_Rotated_Sorted_Array.java
@@ -17,12 +17,41 @@ public class _3_Search_in_Rotated_Sorted_Array extends BaseProblem implements IP
 
     @Override
     public void run() {
-
+        int[] a = new int[]{4, 5, 6, 7, 0, 1, 2};
+        a = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 99, 100, 101};
+        print(new Solution().search(a, 101));
     }
 
     public class Solution {
         public int search(int[] A, int target) {
-            return 0;
+            return find(A, 0, A.length - 1, target);
+        }
+
+        private int find(int[] A, int start, int end, int target) {
+            if (end < start) return -1;
+            int middle = start + (end - start) / 2;
+            if (A[middle] == target) return middle;
+            else if (target < A[middle])
+                return find(A, start, middle - 1, target);
+            else
+                return find(A, middle + 1, end, target);
+
+//            if (start > end) return -1;
+//            int middle = start + (end - start) / 2;
+//            if (target == A[middle]) return middle;
+//            if (A[start] < A[middle]) {
+//                if (target < A[middle]) {
+//                    return find(A, start, middle - 1, target);
+//                } else {
+//                    return find(A, middle + 1, end, target);
+//                }
+//            } else {
+//                if (target > A[middle]) {
+//                    return find(A, middle + 1, end, target);
+//                } else {
+//                    return find(A, start, middle - 1, target);
+//                }
+//            }
         }
     }
 }
