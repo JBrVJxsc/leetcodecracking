a8e35cd84a957d6632118ede10f1c82db909f293
laonawuli <laonawuli@126.com>
2014-07-24 13:16:13 +0800

    Code

---
+++ src/com/leetcode/onlinejudge/problems/_4_Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,21 @@
+package com.leetcode.onlinejudge.problems;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+
+/**
+ * Created by Who on 2014/7/24.
+ */
+public class _4_Search_in_Rotated_Sorted_Array_II extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Follow up for \"Search in Rotated Sorted Array\": What if duplicates are allowed?\n" +
+                "Would this affect the run-time complexity? How and why?\n" +
+                "Write a function to determine if a given target is in the array.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
