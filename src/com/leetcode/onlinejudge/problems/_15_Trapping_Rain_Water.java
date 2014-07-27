d39076b4b75118de2ceb27da333f28adb88d4488
laonawuli <laonawuli@126.com>
2014-07-27 11:04:18 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_15_Trapping_Rain_Water.java
+++ src/com/leetcode/onlinejudge/problems/_15_Trapping_Rain_Water.java
@@ -24,7 +24,95 @@ public class _15_Trapping_Rain_Water extends BaseProblem implements IProblem {
 
     public class Solution {
         public int trap(int[] A) {
-            return 0;
+            if (A == null || A.length == 0) return 0;
+            int total = 0;
+            int[] left = new int[A.length];
+            int[] right = new int[A.length];
+            int leftMax = left[0] = A[0];
+            int rightMax = right[A.length - 1] = A[A.length - 1];
+            for (int i = 1; i < A.length; i++) {
+                leftMax = Math.max(leftMax, A[i]);
+                left[i] = leftMax;
+                rightMax = Math.max(rightMax, A[A.length - 1 - i]);
+                right[A.length - 1 - i] = rightMax;
+            }
+            for (int i = 0; i < A.length; i++)
+                total += Math.min(left[i], right[i]) - A[i];
+            return total;
+        }
+
+        public int trap1(int[] A) {
+            if (A == null || A.length == 0) return 0;
+            int total = 0;
+            int[] left = new int[A.length];
+            int[] right = new int[A.length];
+            int leftMax = left[0] = A[0];
+            int rightMax = right[A.length - 1] = A[A.length - 1];
+            for (int i = 1; i < A.length; i++) {
+                leftMax = Math.max(leftMax, A[i]);
+                left[i] = leftMax;
+                rightMax = Math.max(rightMax, A[A.length - 1 - i]);
+                right[A.length - 1 - i] = rightMax;
+            }
+            for (int i = 0; i < A.length; i++) {
+                total += Math.min(left[i], right[i]) - A[i];
+            }
+            return total;
+        }
+
+        public int trap2(int[] A) {
+            int total = 0;
+            int[] left = new int[A.length];
+            int[] right = new int[A.length];
+            int leftMax = left[0] = A[0];
+            int rightMax = right[A.length - 1] = A[A.length - 1];
+            for (int i = 1; i < A.length; i++) {
+                leftMax = Math.max(leftMax, A[i]);
+                left[i] = leftMax;
+                rightMax = Math.max(rightMax, A[A.length - 1 - i]);
+                right[A.length - 1 - i] = rightMax;
+            }
+            for (int i = 0; i < A.length; i++) {
+                total += Math.min(left[i], right[i]) - A[i];
+            }
+            return total;
+        }
+
+        public int trap3(int[] A) {
+            if (A == null || A.length == 0) return 0;
+            int total = 0;
+            int[] left = new int[A.length];
+            int[] right = new int[A.length];
+            int leftMax = left[0] = A[0];
+            int rightMax = right[A.length - 1] = A[A.length - 1];
+            for (int i = 1; i < A.length; i++) {
+                leftMax = Math.max(leftMax, A[i]);
+                left[i] = leftMax;
+                rightMax = Math.max(rightMax, A[A.length - 1 - i]);
+                right[A.length - 1 - i] = rightMax;
+            }
+            for (int i = 0; i < A.length; i++) {
+                total += Math.min(left[i], right[i]) - A[i];
+            }
+            return total;
+        }
+
+        public int trap4(int[] A) {
+            if (A == null || A.length == 0) return 0;
+            int total = 0;
+            int[] left = new int[A.length];
+            int[] right = new int[A.length];
+            int leftMax = left[0] = A[0];
+            int rightMax = right[A.length - 1] = A[A.length - 1];
+            for (int i = 1; i < A.length; i++) {
+                leftMax = Math.max(leftMax, A[i]);
+                left[i] = leftMax;
+                rightMax = Math.max(rightMax, A[A.length - 1 - i]);
+                right[A.length - 1 - i] = rightMax;
+            }
+            for (int i = 0; i < A.length; i++)
+                total += Math.min(left[i], right[i]) - A[i];
+            return total;
         }
     }
 
