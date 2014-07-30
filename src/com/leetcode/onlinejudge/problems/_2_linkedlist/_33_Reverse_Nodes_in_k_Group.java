4496053c07e53971fa8af3fc891094d148ecc693
laonawuli <laonawuli@126.com>
2014-07-30 14:53:59 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_2_linkedlist/_33_Reverse_Nodes_in_k_Group.java
+++ src/com/leetcode/onlinejudge/problems/_2_linkedlist/_33_Reverse_Nodes_in_k_Group.java
@@ -28,7 +28,43 @@ public class _33_Reverse_Nodes_in_k_Group extends BaseProblem implements IProble
         print(new Solution().reverseKGroup(l2, 3));
     }
 
+    // A better solution.
     public class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            if (head == null || k == 1) {
+                return head;
+            }
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode pre = dummy;
+            int i = 0;
+            while (head != null) {
+                i++;
+                if (i % k == 0) {
+                    pre = reverse(pre, head.next);
+                    head = pre.next;
+                } else {
+                    head = head.next;
+                }
+            }
+            return dummy.next;
+        }
+
+        public ListNode reverse(ListNode pre, ListNode next) {
+            ListNode last = pre.next;
+            ListNode cur = last.next;
+            while (cur != next) {
+                last.next = cur.next;
+                cur.next = pre.next;
+                pre.next = cur;
+                cur = last.next;
+            }
+            return last;
+        }
+    }
+
+    // My stupid solution.
+    public class Solution1 {
         public ListNode reverseKGroup(ListNode head, int k) {
             if (head == null || k < 2) return head;
             int counter = 0;
