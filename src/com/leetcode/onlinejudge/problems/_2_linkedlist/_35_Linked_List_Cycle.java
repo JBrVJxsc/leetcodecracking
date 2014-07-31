d0ffb09529b03c3d2461e8fddb712d14b5b0e6ec
laonawuli <laonawuli@126.com>
2014-07-31 12:19:53 +0800

    Code

---
+++ src/com/leetcode/onlinejudge/problems/_2_linkedlist/_35_Linked_List_Cycle.java
@@ -0,0 +1,28 @@
+package com.leetcode.onlinejudge.problems._2_linkedlist;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+import com.leetcode.onlinejudge.common.ListNode;
+
+/**
+ * Created by Who on 2014/7/31.
+ */
+public class _35_Linked_List_Cycle extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Given a linked list, determine if it has a cycle in it.\n" +
+                "Follow up: Can you solve it without using extra space?";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+
+            return true;
+        }
+    }
+}
