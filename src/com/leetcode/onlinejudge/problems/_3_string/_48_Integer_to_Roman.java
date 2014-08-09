62deaad013e0c3cbb66086e0958240e2561f2aac
laonawuli <laonawuli@126.com>
2014-08-09 11:09:22 +0800

    Code

---
+++ src/com/leetcode/onlinejudge/problems/_3_string/_48_Integer_to_Roman.java
@@ -0,0 +1,26 @@
+package com.leetcode.onlinejudge.problems._3_string;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+
+/**
+ * Created by Who on 2014/8/9.
+ */
+public class _48_Integer_to_Roman extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Given an integer, convert it to a roman numeral.\n" +
+                "Input is guaranteed to be within the range from 1 to 3999.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class Solution {
+        public String intToRoman(int num) {
+            return null;
+        }
+    }
+}
---
+++ src/com/leetcode/onlinejudge/problems/_3_string/_49_Roman_to_Integer.java
@@ -0,0 +1,26 @@
+package com.leetcode.onlinejudge.problems._3_string;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+
+/**
+ * Created by Who on 2014/8/9.
+ */
+public class _49_Roman_to_Integer extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Given a roman numeral, convert it to an integer.\n" +
+                "Input is guaranteed to be within the range from 1 to 3999.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class Solution {
+        public int romanToInt(String s) {
+            return 0;
+        }
+    }
+}
