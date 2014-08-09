a027ea8c0ca87167c6e147d451a3da42e9b157a9
laonawuli <laonawuli@126.com>
2014-08-09 22:35:57 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_3_string/_52_Simplify_Path.java
+++ src/com/leetcode/onlinejudge/problems/_3_string/_52_Simplify_Path.java
@@ -3,6 +3,8 @@ package com.leetcode.onlinejudge.problems._3_string;
 import com.leetcode.interfaces.IProblem;
 import com.leetcode.onlinejudge.BaseProblem;
 
+import java.util.Stack;
+
 /**
  * Created by Who on 2014/8/9.
  */
@@ -22,12 +24,40 @@ public class _52_Simplify_Path extends BaseProblem implements IProblem {
 
     @Override
     public void run() {
-
+        print(new Solution().simplifyPath("/home/"));
+        print(new Solution().simplifyPath("/a/./b/../../c/"));
+        print(new Solution().simplifyPath("/../"));
+        print(new Solution().simplifyPath("/home//foo/"));
     }
 
     public class Solution {
+        // 384ms for 252 test cases
         public String simplifyPath(String path) {
-            return null;
+            if (path == null)
+                return null;
+            if (path.length() == 0)
+                return "/";
+            int n = path.length();
+            Stack<String> stack = new Stack<String>();
+            String[] directories = path.split("/");     // Separate path by "/"
+            for (String dir : directories) {
+                if (dir.length() != 0) {
+                    if (dir.equals("..")) {     // Go up to its parent
+                        if (!stack.empty())
+                            stack.pop();
+                    } else if (!dir.equals(".")) {  // Enter its subdirectory
+                        stack.push(dir);
+                    }
+                }
+                // No redundant "/"
+            }
+            // Construct simplified path
+            if (stack.empty())  // No subdirectory
+                return "/";
+            StringBuilder result = new StringBuilder();
+            for (String s : stack.toArray(new String[stack.size()]))
+                result.append("/" + s);
+            return result.toString();
         }
     }
 }
