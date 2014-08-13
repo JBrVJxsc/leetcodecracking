5a2660a1d9f2232a17513ca887555aa056ab795f
laonawuli <laonawuli@126.com>
2014-08-13 20:50:03 +0800

    Code

---
+++ src/com/leetcode/onlinejudge/problems/_5_tree/_71_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -0,0 +1,26 @@
+package com.leetcode.onlinejudge.problems._5_tree;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+import com.leetcode.onlinejudge.common.TreeNode;
+
+/**
+ * Created by Who on 2014/8/13.
+ */
+public class _71_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Given inorder and postorder traversal of a tree, construct the binary tree.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class Solution {
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            return null;
+        }
+    }
+}
