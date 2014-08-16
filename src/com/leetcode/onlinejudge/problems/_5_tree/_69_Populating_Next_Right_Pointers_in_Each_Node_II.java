95d2c0f473c734a644fa0d14b23963f05e6a5a4d
laonawuli <laonawuli@126.com>
2014-08-16 16:18:45 +0800

    Code

--- src/com/leetcode/onlinejudge/problems/_5_tree/_69_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ src/com/leetcode/onlinejudge/problems/_5_tree/_69_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -8,38 +8,31 @@ import com.leetcode.onlinejudge.common.TreeLinkNode;
  * Created by Who on 2014/8/12.
  */
 public class _69_Populating_Next_Right_Pointers_in_Each_Node_II extends BaseProblem implements IProblem {
+
     @Override
     public String getSummary() {
-        return "Given a binary tree\n" +
-                "    struct TreeLinkNode {\n" +
-                "      TreeLinkNode *left;\n" +
-                "      TreeLinkNode *right;\n" +
-                "      TreeLinkNode *next;\n" +
-                "    }\n" +
-                "Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.\n" +
-                "Initially, all next pointers are set to NULL.\n" +
+        return "Follow up for problem \"Populating Next Right Pointers in Each Node\".\n" +
+                "What if the given tree could be any binary tree? Would your previous solution still work?\n" +
                 "Note:\n" +
                 "You may only use constant extra space.\n" +
-                "You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).\n" +
                 "For example,\n" +
-                "Given the following perfect binary tree,\n" +
+                "Given the following binary tree,\n" +
                 "         1\n" +
                 "       /  \\\n" +
                 "      2    3\n" +
-                "     / \\  / \\\n" +
-                "    4  5  6  7\n" +
+                "     / \\    \\\n" +
+                "    4   5    7\n" +
                 "After calling your function, the tree should look like:\n" +
                 "         1 -> NULL\n" +
                 "       /  \\\n" +
                 "      2 -> 3 -> NULL\n" +
-                "     / \\  / \\\n" +
-                "    4->5->6->7 -> NULL";
+                "     / \\    \\\n" +
+                "    4-> 5 -> 7 -> NULL";
     }
 
     @Override
     public void run() {
-        TreeLinkNode node = new TreeLinkNode(0);
-        new Solution().connect(node);
+
     }
 
     public class Solution {
@@ -47,22 +40,30 @@ public class _69_Populating_Next_Right_Pointers_in_Each_Node_II extends BaseProb
             if (root == null) {
                 return;
             }
-            connect(root.left);
+            if (root.left != null) {
+                if (root.right != null) {
+                    root.left.next = root.right;
+                } else {
+                    TreeLinkNode node = root.next;
+                    while (node != null && node.left == null && node.right == null) {
+                        node = node.next;
+                    }
+                    if (node != null) {
+                        root.left.next = node.left == null ? node.right : node.left;
+                    }
+                }
+            }
+            if (root.right != null) {
+                TreeLinkNode node = root.next;
+                while (node != null && node.left == null && node.right == null) {
+                    node = node.next;
+                }
+                if (node != null) {
+                    root.right.next = node.left == null ? node.right : node.left;
+                }
+            }
             connect(root.right);
-            if (root.left == null) {
-                return;
-            }
-            root.left.next = root.right;
-            if (root.left.right == null) {
-                return;
-            }
-            TreeLinkNode right = root.left.right;
-            TreeLinkNode left = root.right.left;
-            while (right != null) {
-                right.next = left;
-                right = right.right;
-                left = left.left;
-            }
+            connect(root.left);
         }
     }
 }
---
+++ src/com/leetcode/onlinejudge/problems/_6_sort/_87_Insertion_Sort_List.java
@@ -0,0 +1,26 @@
+package com.leetcode.onlinejudge.problems._6_sort;
+
+import com.leetcode.interfaces.IProblem;
+import com.leetcode.onlinejudge.BaseProblem;
+import com.leetcode.onlinejudge.common.ListNode;
+
+/**
+ * Created by Who on 2014/8/16.
+ */
+public class _87_Insertion_Sort_List extends BaseProblem implements IProblem {
+    @Override
+    public String getSummary() {
+        return "Sort a linked list using insertion sort.";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public class Solution {
+        public ListNode insertionSortList(ListNode head) {
+            return null;
+        }
+    }
+}
